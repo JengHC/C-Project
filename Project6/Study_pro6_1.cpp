0a1d381e5d3df99a11f27d007be233ef94a83552
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2023-03-06 20:49:02 +0900

    따배씨++ Chapter1.6

---
+++ Project6/Study_pro6_1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+//키워드와 식별자에 관련된 내용 
+void getNumber() //.....
+{
+
+}
+
+class MyClass
+{
+public:
+	int variable;
+};
+
+
+int main()
+{
+	int n_apples;
+	int value;
+	int _apples;
+
+	getNumber();
+}
