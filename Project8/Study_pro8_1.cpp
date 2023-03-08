2e89622229cfff892f5b819a1eb2496b8a2d7bd9
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2023-03-08 21:54:27 +0900

    Chapter 1.8

---
+++ Project8/Study_pro8_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x = -2; // x 는 변수(variable), 2는 리터럴(데이터 그 자체)
+
+	cout << 1 + 2 << endl; // x + 2 는 피연산자(Opernd)
+
+	int y = (x > 0) ? 1 : 2; // 삼항 연산자
+
+	cout << y << endl;
+
+	cout << "My Home" << endl;
+
+	return 0;
+}
