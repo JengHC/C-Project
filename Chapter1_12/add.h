6d7315024c74775608aecf405b9187fd73959e47
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2023-03-09 20:55:26 +0900

    Chapter 1.12

---
+++ Chapter1_12/add.h
@@ -0,0 +1,11 @@
+#pragma once
+//#ifndef  MY_ADD << 이 전처리기는 #pragma once와 같은 역할을 함.
+//#define MY_ADD
+
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
+//#endif // ! MY_ADD
---
+++ Chapter1_12/my_work.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "add.h"
+
+void doSomthing()
+{
+	add(1, 2);
+}
