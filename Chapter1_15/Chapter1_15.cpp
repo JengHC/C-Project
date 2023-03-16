3dad1e6e2b82a4ef25f1d454ac470e926d19cb4f
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2023-03-16 16:53:21 +0900

    Chapter 1.15

--- Chapter1_15/Chapter1_15.cpp
+++ Chapter1_15/Chapter1_15.cpp
@@ -5,5 +5,28 @@ using namespace std;
 int main()
 {
     
+    bool bValue = false;
+    char chValue = '65';
+    float fValue = 3.141592f;
+    double dValue = 3.141592;
+
+    int i = (int)3.1415;// copy initialization
+    int a((int)3.14);   // direct initialization
+    int b{ 123 };   // uniform initialization
+
+    //int k, l, m = 123; //이런식으로 초기화 하면 안됨.
+
+    int k = 0, l = 456, m = 123; //int k = 0, l(456), m{123}; 이렇게 사용가능
+
+    /*cout << sizeof(bool) << endl;
+    cout << sizeof(bValue) << endl;*/
+
+    /*auto aValue = 3.141592;
+    auto aValue2 = 3.141592f;*/
+
+    /*cout << bValue << endl;
+    cout << sizeof(aValue) << endl;
+    cout << sizeof(aValue2) << endl;*/
+
     return 0;
 }
