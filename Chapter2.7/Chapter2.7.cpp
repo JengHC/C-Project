135de235c06c90d580d85e8081cb9106b94e301c
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2023-03-22 22:02:04 +0900

    Chapter2.7

--- Chapter2.7/Chapter2.7.cpp
+++ Chapter2.7/Chapter2.7.cpp
@@ -1,20 +1,58 @@
-﻿// Chapter2.7.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
+﻿#include<iostream>
+#include<limits>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	char c1(65); //c1 = 65; c1{65};
+	//char c2('A'); // "hello, world" 문자 하나를 사용할때는 '' , 문자열을 사용할때 ""
+
+	//cout << c1 << " " << c2 << " " << int(c1) << " " << int(c2) << endl;
+
+	//// c-style casting
+	//cout << (char)65 << endl;
+	//cout << (int)'A' << endl;
+	//
+	//// cpp style
+	//cout << char(65) << endl;
+	//cout << int('A') << endl;
+
+	//cout << static_cast<char>(65) << endl; // 65를 char 로 변경 => A
+	//cout << static_cast<int>('A') << endl; // A를 int 로 변경  => 65
+
+	//char ch(97);
+	//cout << ch << endl;
+
+	//cout << static_cast<int>(ch) << endl;
+	//cout << ch << endl;
+
+	//cin >> c1;
+	//cout << c1 << " " << static_cast<int>(c1) << endl; 
+
+	//cin >> c1;
+	//cout << c1 << " " << static_cast<int>(c1) << endl;
+
+	/*cout << sizeof(char) << endl;
+	cout << (int)std::numeric_limits<char>::max() << endl;
+	cout << (int)std::numeric_limits<char>::lowest() << endl;
+
+	cout << sizeof(unsigned char) << endl;
+	cout << (int)std::numeric_limits<unsigned char>::max() << endl;
+	cout << (int)std::numeric_limits<unsigned char>::lowest() << endl;*/
+
+	/*cout << int('\n') << endl; //\n 줄 변경
+	cout << "This is first line \n second line\n";
+	cout << "This is first line" << endl;
+	cout<< "second line";*/
+
+	//cout << int('\t') << endl;
+	//cout << "this is first line \t second line"; //\t 탭
+
+	cout << int('\t') << endl;
+	cout << "this is first line \a second line " ; // \a 소리
+
+	wchar_t c; // 잘안씀
+	char32_t c3;
+	
+	return 0;
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
