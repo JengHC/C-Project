dec1c445641aa1018e6e95e5e5dd6c79ff46c7d3
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2023-03-22 21:29:04 +0900

    Chapter2.6

---
+++ Chapter2.6/Chapter2.6.2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a=0;
+
+	cin >> a;
+
+	if (a % 2 == 0)
+	{
+		cout << " Â¦¼ö ÀÔ´Ï´Ù." << endl;
+	}
+	else
+	{
+		cout << " È¦¼ö ÀÔ´Ï´Ù." << endl;
+	}
+
+	return 0;
+}
--- Chapter2.6/Chapter2.6.cpp
+++ Chapter2.6/Chapter2.6.cpp
@@ -1,20 +1,69 @@
-﻿// Chapter2.6.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
+using namespace std;
 
-#include <iostream>
+bool isEqual(int a, int b)
+{
+	bool result = (a == b); // a와 b가 같으면 True , 아니면 False
+	
+	return result;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	//bool b1 = true; // copy initialization
+	//bool b2(false); // direct '''
+	//bool b3{ true }; //uniform ini'''
+	//b3 = false;
+
+	//cout << boolalpha; // 0,1로 출력되던걸 true, flase로 출력
+	//cout << isEqual(1, 1) << endl;
+	//cout << isEqual(0, 3) << endl;
+
+	/*cout << b3 << endl;
+	cout << b1 << endl;*/
+
+	//cout << !true << endl; // ! => not 이라는 뜻 !을 잘 쓰지는 않음
+	//cout << !false << endl;
+
+	//cout << (true && true) << endl; // &&연산자는 모두 True면 True, 하나라도 True가 아니면 False
+	//cout << (true && false) << endl;
+	//cout << (false && true) << endl;
+	//cout << (false && false) << endl;
+
+	//cout << (true || true) << endl; // || (or)연산자는 하나만 True여도 True 
+	//cout << (true || false) << endl;
+	//cout << (false || true) << endl;
+	//cout << (false || false) << endl;
+
+	/*if (false)
+		cout << "this is true" << endl;
+	else
+		cout << "this is false" << endl;*/
+
+	//if (true)
+	//{ 
+	//	cout << "this is true" << endl;
+	//	cout << "True second line" << endl;
+	//}
+	//else
+	//{
+	//	cout << "this is false" << endl;
+	//}
+
+	// if문에 조건제시 할때 True를 1로 해도 무관
+	//if (1) 
+	//	cout << "True" << endl;
+	//else
+	//	cout << "False" << endl;
+
+	bool b;
+	cin >> b;
+	// boolean값을 입력할때 true false로 입력을 하면 안된다. 이럴경우 오류 발생
+	// boolean값을 입력할때는 0,1로 입력 必 
+
+	cout << boolalpha;
+	cout << "Your input: " << b << endl;
+
+	return 0;
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
