a7d544629e679a2ff682b5dffc7ac3d52eb2027d
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2023-03-24 16:59:06 +0900

    Chapter2.8

---
+++ Chapter2.8/Chapter2.8.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	/*unsigned int n = 5u;
+	long n2 = 5L;
+	double d = 6.0e-10;*/
+
+	//Decimal : 0 1 2 3 4 5 6 7 8 9 10
+	//Octal(8): 0 1 2 3 4 5 6 7 10 11 12 13
+	//Hexa(16): 0 1 2 3 4 5 6 7 8 9 A B C D E F 10 
+
+
+	const int price_per_item = 10;
+	int num_items = 123;
+	int price = num_items * price_per_item;
+
+
+	return 0;
+}
