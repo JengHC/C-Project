1be38dd28f07470da2063c6903e662772576108f
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2023-04-24 14:53:47 +0900

    Chapter4.6(std string)

---
+++ Chapter4.6/Chapter4.6.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+#include<string>
+#include<limits>
+using namespace std;
+int main()
+{
+	string a("Hello, World");
+	//string b("World");
+	//string hw = a + b; // append
+
+	//hw += "I'm good";
+
+	cout << a.length() << endl;
+
+	//cout << "Your age? : ";
+	//int age;
+	//cin >> age;
+	////getline(cin, age);
+
+	////std::cin.ignore(32767, '\n');
+	//std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+	//cout << "Your name? : ";
+	//string name;
+	////cin >> name; //빈칸 입력안됨.
+	//std::getline(std::cin, name);
+
+	//cout << name << " " << age << endl;
+
+
+
+	//const char my_strs [] = "Hello, World";
+	//const string my_hello = "Hello, World";
+
+	//cout << my_hello << endl;
+
+	return 0;
+}
