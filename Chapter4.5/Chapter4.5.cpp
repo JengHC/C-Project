da6bb90d7dfb8ddcbee9d1563734306a556e794d
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2023-04-24 14:22:54 +0900

    Chapter4.5(Type conversion)

--- Chapter4.5/Chapter4.5.cpp
+++ Chapter4.5/Chapter4.5.cpp
@@ -1,20 +1,48 @@
-﻿// Chapter4.5.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include<iostream>
+#include<typeinfo>
+#include<iomanip> // setprecision사용
 
-#include <iostream>
+// 암시적 형변환 Implicit Type Conversion(coersion)
+    // numeric promotion
+    // numeric conversion
 
+// 명시적 형변환 Explicit Type Conversion(casting)
 int main()
 {
-    std::cout << "Hello World!\n";
+    using namespace std;
+
+    //int a = 123;
+    
+
+    // float가 double 보다 작은데, 작게 사용하는 데이터 타입을 큰걸로 보낼때는
+    // 넓은 곳으로 보내야함(작은곳으로 가면 안됨). 그래서 double d = a 를 사용
+    // 숫자 승격(numeric promotion)
+    //float a = 1.0f;
+    //double d = a; 
+
+    // 숫자 변환(numeric conversion)
+    cout << 5u - 10;
+    // 우선순위 
+    // int < unsigned int < long < unsigned long,
+    // long long < unsigned long long < float < double
+    // long double
+
+    // 명시적 형변환
+    int i = static_cast<int>(4.0);
+
+
+    //double d = 0.123456789;
+    //float f = d;
+
+    //cout << std::setprecision(12) << f << endl;
+
+    //int i = 30000;
+    //char c = i;
+    //// char은 문자기때문에 확인이 불가, 그래서 static으로 명시적 형변환 사용
+    //// int 값이 char(범위 -128~127)보다 커서 출력시 값이 제대로 안나옴
+    //cout << static_cast<int>(c) << endl;
+
+    //cout << typeid(a).name() << endl;
+
+    return 0;
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
