c938fbc4024f443f60bd9c49c30bd0b7367f6c18
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2023-04-27 18:08:36 +0900

    Chapter4.8(Enum Class)

---
+++ Chapter4.8/Chapter4.8.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+
+int main()
+{
+	using namespace std;
+
+	enum class Color
+	{
+		RED,
+		BLUE
+	};
+	enum class Fruit
+	{
+		BANANA,
+		APPLE,
+	};
+
+	Color color1 = Color::BLUE;
+	Color color2 = Color::BLUE;
+
+	if (color1 == color2)
+		cout << "Same color " << endl;
+
+	/*Color color = Color::RED;
+	Fruit fruit = Fruit::BANANA;
+
+	if (static_cast<int>(color) == static_cast<int>(fruit)
+		cout << "Color is fruit? " << endl;*/
+
+	return 0;
+}
