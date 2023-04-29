539535f0cec714c21f3b626fe264c20f880f5d6c
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2023-04-29 20:54:04 +0900

    Chapter4.9

--- Chapter4.9/Chapter4.9.cpp
+++ Chapter4.9/Chapter4.9.cpp
@@ -1,10 +1,28 @@
 ﻿#include<iostream>
 #include<Vector>
+#include<cstdint>
 using namespace std;
 
 int main()
 {
-	
+	//typedef double distance_t;
+
+	//std::int8_t i(97); //고정너비정수
+
+	//typedef double distance_t; //이떄 double을 float으로 바꾸면 distance_t가 float형으로 변경
+
+	//double     my_distance;
+	//distance_t home2work;
+	//distance_t home2school;
+
+	typedef vector<pair<string, int>>pairlist_t;
+
+	//vector < pair<string, int> > pairlist1;
+	//vector < pair<string, int> > pairlist2;
+
+	pairlist_t pairlist1;
+	pairlist_t pairlist2;
+
 
 	return 0;
 }
