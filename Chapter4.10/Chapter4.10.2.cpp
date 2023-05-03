8ee70533ac71df2ac641f3e5a3aa734235ae3451
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2023-05-03 16:00:56 +0900

    Chapter4.10(Struct)

--- Chapter4.10/Chapter4.10.2.cpp
+++ Chapter4.10/Chapter4.10.2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 // 출력값을 보면 16이 나온다.
 // id,age,wage를 다 계산하면 이론상 14이지만,
 // 컴퓨터의 데이터 구조상 2bytes를 잘 인식 하지 못하기 때문에 
-// id,공백,age,wage 구조로 받아들여 2 + 2 + 4 + 8 = 16 이라는 결과가 나온다
+// id,공백,age,wage 구조로 받아들여 padding되어서 2 + (2) + 4 + 8 = 16 이라는 결과가 나온다
 //  https://en.wikipedia.org/wiki/Data_structure_alignment
 struct Employee   // 2 + 4 + 8 =14
 {
