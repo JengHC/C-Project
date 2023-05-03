f9f6c6fc64cad3a2be8baaeeb7bcf73e1e64c8ce
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2023-05-03 16:00:08 +0900

    Chapter4.10(Struct)

---
+++ Chapter4.10/Chapter4.10.2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+
+// 출력값을 보면 16이 나온다.
+// id,age,wage를 다 계산하면 이론상 14이지만,
+// 컴퓨터의 데이터 구조상 2bytes를 잘 인식 하지 못하기 때문에 
+// id,공백,age,wage 구조로 받아들여 2 + 2 + 4 + 8 = 16 이라는 결과가 나온다
+//  https://en.wikipedia.org/wiki/Data_structure_alignment
+struct Employee   // 2 + 4 + 8 =14
+{
+	short  id;    // 2 bytes  
+	int    age;   // 4 bytes
+	double wage;  // 8 bytes
+};
+
+int main()
+{
+	Employee emp1;
+
+	cout << sizeof(Employee) << endl; 
+
+	return 0;
+}
--- Chapter4.10/Chapter4.10.cpp
+++ Chapter4.10/Chapter4.10.cpp
@@ -1,9 +1,53 @@
 ﻿#include<iostream>
 using namespace std;
 
+struct Person
+{
+	double height = 3.0;
+	float  weight = 200.0;
+	int    age = 100;
+	string name = "Mr. Incredible";
+
+	//void print()
+	//{
+	//	cout << height << " " << weight << " " << age << " " << name;
+	//	cout << endl;
+	//}
+};
+
+//Person getMe()
+//{
+//	Person me{ 2.0,100.0,20,"Jeong" };
+//	return me;
+//	
+//}
+
+//struct Family //구조체 안에 구조체를 넣을 수 있다.
+//{
+//	Person me, mom, dad;
+//};
+
 int main()
 {
+	Person me{ 2.0,100.0,20,"Jeong" };
+	//Person me;
+	cout << me.name << endl;
 
+	/*Person me_from_func = getMe();
+	me_from_func.print();*/
+
+	//Person me{2.0,100.0,20,"Jeong"};
+	//printPerson(me);
+	//me.print();
+
+	//Person me2(me);
+	//me2 = me;
+	//me2.print();
+
+
+	//me.age = 26;
+	//me.name = "Jeong";
+	//me.height = 1.78;
 
 	return 0;
 }
