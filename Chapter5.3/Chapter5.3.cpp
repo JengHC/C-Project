13252ac65820aa36607f751aca936893f0fca441
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2023-05-30 16:49:20 +0900

    Chapter5.3(switch-case)

---
+++ Chapter5.3/Chapter5.3.cpp
@@ -0,0 +1,87 @@
+﻿#include<iostream>
+using namespace std;
+
+enum class Colors
+{
+	BLACK,
+	WHITE,
+	RED,
+	GREEN,
+	BLUE
+};
+
+void printColorName(Colors color)
+{
+	//switch (static_cast<int>(color))
+	//{
+	//case 0:
+	//	cout << "Black" << endl;
+	//	break;
+	//case 1:
+	//	cout << "White" << endl;
+	//	break;
+	//}
+
+	//if (color == Colors::BLACK)
+	//{
+	//	cout << "Black" << endl;
+	//}
+	//else if (color == Colors::WHITE)
+	//{
+	//	cout << "White" << endl;
+	//}
+	//else if (color == Colors::RED)
+	//{
+	//	cout << "Red" << endl;
+	//}
+}
+int main()
+{
+	int x;
+	cin >> x;
+
+	switch (x)
+	{
+		case 0:
+		{
+			int y = 5;
+			y = y + x;
+			cout << y << endl;
+			break;
+		}
+		case 1:
+		{
+			int y = 5;
+			y = y - x;
+			cout << y << endl;
+			break;
+		}
+		default:
+			cout << "Undefined input " << x << endl;
+			break;
+
+	}
+
+	//printColorName(Colors::BLACK);
+
+	//int x;
+	//cin >> x;
+
+	//{
+	//	switch (x)
+	//	{
+	//	case 0:
+	//		cout << "Zero";
+	//		//break;
+	//	case 1:
+	//		cout << "One";
+	//		//break;
+	//	case 2:
+	//		cout << "Two";
+	//		//break;
+	//	}
+
+	//	cout << endl;
+	//}
+	return 0;
+}
