dc36971a7fd3c251a19915502bcef226e96ad48c
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2023-05-30 16:57:05 +0900

    Chapter5.4(goto)

---
+++ Chapter5.4/Chapter5.4.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+//  이렇게는 사용 불가
+//	goto skip;
+//
+//	int x = 5;
+//
+//skip:
+//
+//	x += 3;
+//
+//	return 0;
+
+
+	//double x;
+
+//tryAgain: // Lable
+
+	//cout << "Enter a non-negative number" << endl;
+	//cin >> x;
+
+	//if (x < 0.0)
+	//{
+	//	goto tryAgain;
+	//}
+
+	//cout << sqrt(x) << endl;
+
+
+	return 0;
+}
