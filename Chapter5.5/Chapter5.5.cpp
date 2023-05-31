185c0e9e85d06165d5c23fd51dc8e1473cb1d9c6
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2023-05-31 15:07:07 +0900

    Chapter5.5(while)

---
+++ Chapter5.5/Chapter5.5.cpp
@@ -0,0 +1,69 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	//int outer_count = 5;
+
+	//while (outer_count >= 0)
+	//{
+	//	int inner_count = 1;
+	//	while (inner_count <= outer_count)
+	//	{
+	//		cout << inner_count++ << " ";
+
+	//	}
+	//	cout << endl;
+	//	--outer_count;
+	//}
+
+
+	//int outer_count = 1;
+
+	//while (outer_count <= 5)
+	//{
+	//	int inner_count = 1;
+	//	while (inner_count <= outer_count)
+	//	{
+	//		cout << inner_count++ << " ";
+
+	//	}
+	//	cout << endl;
+	//	++outer_count;
+	//}
+
+	//int count = 1;
+
+	//while (count <100)
+	//{
+	//	// 5번에 한번씩 Hello를 출력하고 싶다.
+	//	if (count % 5 == 0)cout << "Hello" << count < endl;
+	//	
+	//	count++;
+	//}
+
+
+	//unsigned int count = 10;
+
+	//while (count >= 0)
+	//{
+	//	if (count == 0) cout << "zero";
+	//	else cout << count << " ";
+
+	//	count--;
+	//}
+
+	//int count = 0;
+	//while (1)
+	//{
+	//	cout << count << endl;
+	//	++count;
+
+	//	if (count == 10)
+	//	{
+	//		break;
+	//	}
+	//}
+	
+	return 0;
+}
