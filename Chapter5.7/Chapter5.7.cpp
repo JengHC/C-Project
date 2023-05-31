767991172b2f38fd277a06994d3881d333064ad9
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2023-05-31 17:37:33 +0900

    Chapter5.7

---
+++ Chapter5.7/Chapter5.7.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	int count = 0;
+
+	for (int count = 0; count < 10; count++) // iteration
+	{
+		cout << count << endl;
+	}
+
+	cout << count << endl;
+
+	return 0;
+}
