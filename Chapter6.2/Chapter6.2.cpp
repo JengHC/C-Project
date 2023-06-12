8fb149c79f4d0f1c9afb05de740d10a77047cb85
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2023-06-12 15:41:51 +0900

    Chapter6.2(array)

---
+++ Chapter6.2/Chapter6.2.cpp
@@ -0,0 +1,66 @@
+﻿#include<iostream>
+using namespace std;
+
+#define NUM_STUDENTS 10000
+
+
+
+//struct  Rectangle
+//{
+//	int length;
+//	int width;
+//};
+
+//enum StudentName
+//{
+//	JACKJACK,     // = 0
+//	DASH,         // = 1
+//	VIOLET,       // = 2
+//	NUM_STUDENTS, // = 3
+//};
+
+int main()
+{
+	
+
+	/*int scores[NUM_STUDENTS];
+
+	scores[JACKJACK] = 0;
+	scores[VIOLET] = 100;
+
+	cout << scores[JACKJACK] << endl;
+	cout << scores[VIOLET] << endl;*/
+
+	/*int my_array[] { 1,2,3,4,5 };
+
+	cout << my_array[JACKJACK] << endl;
+	cout << my_array[4] << endl;*/
+
+
+	/*cout << sizeof(Rectangle) << endl;
+
+	Rectangle rect_arr[10];
+
+	cout << sizeof(rect_arr) << endl;
+
+	rect_arr[0].length = 1;
+	rect_arr[0].width = 1;*/
+
+	//int one_score;         // 1 variable
+	//int student_scores[5]; // 5 int
+
+	//one_score = 100;
+
+	//student_scores[0] = 100; // 1st element
+	//student_scores[1] = 80;  // 2nd element
+	//student_scores[2] = 90;  // 3nd element
+	//student_scores[3] = 50;  // 4nd element
+	//student_scores[4] = 0;   // 5nd element
+
+	//cout << student_scores[0] << endl;
+
+	/*cout << sizeof(one_score) << endl;;
+	cout << sizeof(student_scores) << endl;*/
+
+	return 0;
+}
