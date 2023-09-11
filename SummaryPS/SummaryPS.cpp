0e3f334f28f27765e75ff12622fb6f1e4143a33d
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2023-09-11 14:10:15 +0900

    초 시분초 변경

---
+++ SummaryPS/SummaryPS.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	int h,m,s;
+	cin >> s;
+
+	//switch (s)
+	//{
+
+	m = s / 60;
+	s = s % 60;  // %는 나눈 후, 나머지 반환함.
+
+	h = m / 60; // 1시간일경우 3600초니까, 3600/60 => m,  m/60=> h 
+	m = m % 60; // m/60 후, 나머지 반환
+
+	if (h<1)
+		cout << m << "분" << s << "초" << endl;
+	
+	else
+		cout << h << "시" << m << "분" << s << "초" << endl;
+}
